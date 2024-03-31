127082baa98b6624c832441569cf5af8cc0f9bd1
samsranlrh <A1874588491@163.com>
2024-03-31 17:37:52 +0800

    basic mode

---
+++ src/planning_main.cpp
@@ -0,0 +1,13 @@
+#include "process.h"
+#include <iostream>
+
+using std::cout, std::endl;
+
+int main()
+{
+    cout << "planning start" << endl;
+    Process pro;
+    pro.planProcess();
+    cout << "planning end" << endl;
+    return 0;
+}
